c23e402536f25117cac5c4279efcc2892ad966ff
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-25 15:43:09 -0400

    last bullet point

--- js/challenge.js
+++ js/challenge.js
@@ -1,30 +1,40 @@
+document.addEventListener("DOMContentLoaded", counterUp);
+let count = document.querySelector("#counter");
+const minusBtn = document.querySelector("#minus");
+const plusBtn = document.querySelector("#plus");
+const heartButton = document.querySelector("#heart");
+const pauseButton = document.querySelector("#pause");
+const likes = document.querySelector(".likes");
+const commentList = document.getElementById("list");
+const listItem = document.createElement("li");
+const unorgList = document.createElement('ul')
+const form = document.getElementById('comment-form')
+
+
+
 //See the timer increment every second once the page has loaded.
-
-document.addEventListener("DOMContentLoaded", startCount)
-let count = document.querySelector("#counter")
-
-
-
 function startCount(){
     count.innerText++
     console.log(count.innerText)
 }
+function counterUp(){
+ setInterID = setInterval(startCount, 1000);
+}
 
-const setInter = setInterval(startCount, 1000);
 
+//pause timer interval
+function stopCount(){
+  clearInterval(setInterID);
+}
 
 // minus func - decrease the displayed # in the counter 
- const minusBtn = document.querySelector("#minus")
   minusBtn.addEventListener('click', () => {
     count.innerText--
     console.log(count.innerText)
   })
 
 
-
-
 // plus func- increasing the displayed # in the counter
-  const plusBtn = document.querySelector("#plus")
     plusBtn.addEventListener('click', () => {
       count.innerText++
       console.log(count.innerText)
@@ -32,16 +42,13 @@ const setInter = setInterval(startCount, 1000);
 
 
 
-
 // A "like" button (❤️) that adds a "like" for the number that is currently 
 //displayed by the timer
-
-const heart = document.querySelector("#heart");
-  console.log(heart)
-const likes = document.querySelector(".likes");
-  console.log(likes)
-heart.addEventListener('click', () => {
-   likes.innerText++
+  
+heartButton.addEventListener('click', () => {
+  const li = document.createElement("li")
+   li.innerText = `${count.innerText}`
+   likes.appendChild(li)
 })
 
 /*         
@@ -49,30 +56,21 @@ Pause the counter, which should:
  1. pause the counter
  2. disable all buttons except the pause button
  3. switch the label on the button from "pause" to "resume"
-
-*/
-
-const pause = document.querySelector("#pause")
-  console.log(pause)
-
- pause.addEventListener('click', () => {
-    const buttons = document.getElementsByTagName("button")
-    for(const button of buttons){
-        if (pause.id !== button.id){
-        button.disabled = true;
-    } 
-  }
-    clearInterval(setInter) 
-    pause.innerText = 'resume' 
-})
-
-
-
 // Click the "resume" button to restart the counter and re-enable the buttons.
 // target the #pause button with value of 'resume'
+*/
 
 
-// target pause button labeled 'resume'
+ pauseButton.addEventListener('click', () => {
+    if(pauseButton.innerText == "pause"){
+      stopCount();
+      pauseButton.innerText = "resume"
+    }else if (pauseButton.innerText == "resume"){
+      counterUp();
+      pauseButton.innerText = "pause"
+      ;
+    }
+  })
 
 
 
@@ -81,18 +79,14 @@ const pause = document.querySelector("#pause")
 // Then innerText or append child to div with Comments with #list .comments
 
 
-const commentList = document.getElementById("list")
-const paraList = document.createElement("p")
 
 
-const form = document.getElementById('comment-form')
+
 form.addEventListener("submit", (e) => {
      e.preventDefault();
     let commentValue = document.querySelector("#comment-input").value
-    paraList.innerText = commentValue
-    commentList.appendChild(paraList)
+    listItem.innerText = commentValue
+    commentList.appendChild(listItem)
     console.log(commentValue)
 })
-
-
 
